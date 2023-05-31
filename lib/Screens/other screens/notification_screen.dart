acd2ff4637b2aae7ff9a945dd75b5c459feffe1b
BIBS23 <bibinoottukulathil@gmail.com>
2023-05-31 18:43:45 +0530

    push

--- lib/Screens/other screens/notification_screen.dart
+++ lib/Screens/other screens/notification_screen.dart
@@ -14,81 +14,81 @@ class _NotificationPageState extends State<NotificationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         backgroundColor: const Color.fromRGBO(62, 202, 59, 100),
+        backgroundColor: const Color.fromRGBO(62, 202, 59, 100),
         title: const Text('Notification',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold)),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         elevation: 0,
         leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
             },
-            icon: const Icon(Icons.arrow_back,
-                color: Colors.white)),
+            icon: const Icon(Icons.arrow_back, color: Colors.white)),
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('notification').snapshots(),
-        builder: (context,AsyncSnapshot <QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
+          stream:
+              FirebaseFirestore.instance.collection('notification').snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
               return const Center(child: CupertinoActivityIndicator());
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-            final DocumentSnapshot documentSnapshot = snapshot.data!.docs[index];
-            return Padding(
-              padding: const EdgeInsets.all(15),
-              child: Container(
-                height: 150,
-                width: 160,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 0.1,
-                    blurRadius: 7,
-                    )
-                  ]
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset('assets/grant.png',height: 55,width: 55),
-                        const Text('\t\t\tGrant\nAvailable')
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(documentSnapshot['exp'].toString()),
-                        Container(
-                          height: 65,
-                          width: 130,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                              width: 0.8
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      snapshot.data!.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Container(
+                      height: 150,
+                      width: 160,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 0.1,
+                              blurRadius: 7,
                             )
+                          ]),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image.asset('assets/grant.png',
+                                  height: 55, width: 55),
+                              const Text('\t\t\tGrant\nAvailable')
+                            ],
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(3),
-                            child: Center(child: Text(documentSnapshot['grant'],textAlign: TextAlign.justify,)),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text('Exp :\t${documentSnapshot['exp']}'),
+                              Container(
+                                height: 65,
+                                width: 130,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(width: 0.8)),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Center(
+                                      child: Text(
+                                    documentSnapshot['grant'],
+                                    textAlign: TextAlign.justify,
+                                  )),
+                                ),
+                              )
+                            ],
                           ),
-                        )
-                      ],
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
-            );
-          });
-  }),
+                  );
+                });
+          }),
     );
   }
 }
